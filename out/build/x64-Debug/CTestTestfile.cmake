58cb1cd06ea382ff86d7be9c4cfd8e90a950f140
BoryaZhivotnoe <ozzymozzy34@gmail.com>
2024-09-22 18:54:19 +0300

    Зафиксировал

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Documents/все лабы/lab7/CMyArray
+# Build directory: C:/Users/user/Documents/все лабы/lab7/CMyArray/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[CMyArrayTest]=] "C:/Users/user/Documents/все лабы/lab7/CMyArray/bin/CMyArrayTest.exe")
+set_tests_properties([=[CMyArrayTest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/user/Documents/все лабы/lab7/CMyArray/CMakeLists.txt;29;add_test;C:/Users/user/Documents/все лабы/lab7/CMyArray/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
